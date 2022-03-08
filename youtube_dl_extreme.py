6e55e72c6b8f962db28030c9016c7a114539e439
Janmeja Heir <jheir@google.com>
2022-03-08 10:33:50 -0800

    update

--- youtube_dl_extreme.py
+++ youtube_dl_extreme.py
@@ -60,7 +60,7 @@ import subprocess
 import argparse
 import json
 import logging
-#import youtube_dl
+import youtube_dl
 import yt_dlp
 from tqdm import tqdm
 monofix = False
@@ -115,7 +115,7 @@ YDL_COMMON_OPTS = {'restrictfilenames': True,
                    'logger': log,
                    'subtitleslangs': ['en'],
                    'format': 'bestvideo+bestaudio/best'}
-YDL_OPTS_SPECIFIC_RES = {'format': 'bestvideo[width={width}][height={height}][ext=mp4]+bestaudio'.format(width=WIDTH, height=HEIGHT)}
+YDL_OPTS_SPECIFIC_RES = {'format': 'bestvideo[width={width}][height={height}]+bestaudio'.format(width=WIDTH, height=HEIGHT)}
 YDL_OPTS_BEST_RES = {'format': 'bestvideo+bestaudio/best'}
 FFMPEG_MP4_CONTAINER = ['ffmpeg', '-i',
                         '{inpath}', '-ss', '{startpoint}',
@@ -302,11 +302,11 @@ def download_video(url, captions, auto_captions):
 
 
     while True:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             try:
                 ydl.download([url])
                 break
-            except yt_dlp.utils.DownloadError as e:
+            except youtube_dl.utils.DownloadError as e:
                 if 'format not available' in str(e):
                     log.warning('Resolution {res} not available, downloading best possible resolution.'.format(res=args.res))
                     ydl_opts.update(YDL_OPTS_BEST_RES)
